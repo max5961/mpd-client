0ac960e4479402108e75583c59c3343f0c0b6c62
Maxwell Morrissey <mmorrissey92@proton.me>
2025-02-26 23:52:47 -0500

    Modify MPDClient to handle refused connections. - Handles refused connections on start and continues to poll until   connection is granted - Does not currently handle lost connections after connection has   already been gained

--- src/mpd_client/MPDClient.ts
+++ src/mpd_client/MPDClient.ts
@@ -6,21 +6,48 @@ import State from "./composed_classes/State.js";
 import initialStatus from "./initialStatus.js";
 
 export default class MPDClient {
-    private client: Client;
-    private data: Data;
+    private client!: Client;
+    private data!: Data;
+    private handler!: Handler | null;
 
-    public connection: Connection;
-    public command: Command;
-    public state: State;
+    public connection!: Connection;
+    public command!: Command;
+    public state!: State;
+
+    constructor() {
+        // this.listen method publicly defines a handler and it is cached in case
+        // connection is initially refused and gained later.  This would be the case
+        // if you started the client while mpd was not running, and then started
+        // mpd later
+        this.handler = null;
+        this.connect();
+    }
 
     // MPD runs a TCP server on 6600
     // see - https://mpd.readthedocs.io/en/latest/client.html
-    constructor() {
+    private async connect(refusedConnections: number = 0): Promise<boolean | void> {
         this.client = net.createConnection({ port: 6600 });
-        this.data = { status: initialStatus };
-        this.connection = new Connection({ client: this.client, data: this.data });
-        this.command = new Command({ client: this.client, data: this.data });
-        this.state = new State({ client: this.client, data: this.data });
+
+        this.client.on("connect", () => {
+            this.client = net.createConnection({ port: 6600 });
+            this.data = { status: initialStatus };
+            this.connection = new Connection({ client: this.client, data: this.data });
+            this.command = new Command({ client: this.client, data: this.data });
+            this.state = new State({ client: this.client, data: this.data });
+
+            this.handler && this.listen(this.handler!);
+
+            return true;
+        });
+
+        this.client.on("error", () => {
+            // Attempt connection every 500ms until connection gained.
+            setTimeout(() => {
+                this.handleRefusedConnection(++refusedConnections);
+            }, 500);
+
+            return false;
+        });
     }
 
     private isChanged(msg: string): boolean {
@@ -75,42 +102,74 @@ export default class MPDClient {
         handler: Handler,
         opts?: { polling?: boolean; pollingInterval?: number },
     ): void {
+        this.handler = handler;
+
         opts = opts ?? {};
         opts.polling = opts.polling ?? true;
         opts.pollingInterval = opts.pollingInterval ?? 500;
 
         let calls = 0;
         let timeout: NodeJS.Timeout | undefined;
+        let refusedConnection = 0;
 
-        this.client.on("data", (buffer: Buffer) => {
-            // Request status update on initial run
-            if (!calls++) {
-                return this.command.requestStatus();
-            }
+        try {
+            this.client.on("data", (buffer: Buffer) => {
+                const msg = buffer.toString("utf-8");
 
-            // Update status object based on message MPD sent
-            const msg = buffer.toString("utf-8");
-            this.updateStatus(msg);
+                // Request status update on initial run
+                if (!calls++) {
+                    if (!msg.includes("OK MPD")) {
+                        throw new Error("Port 6600 already in use");
+                    }
 
-            // MPD changed, but has only notified us of the change.  Request the
-            // status and don't run handler until the status is received.
-            if (this.isChanged(msg)) {
-                return this.responseOkay(msg) && this.command.requestStatus();
-            }
+                    refusedConnection++;
+                    return this.command.requestStatus();
+                }
 
-            // Run callback with the new status
-            handler(this.data.status);
+                // Update status object based on message MPD sent
+                this.updateStatus(msg);
 
-            // Request the status on the given interval, or idle and wait for messages
-            if (opts.polling && this.state.isPlaying()) {
-                timeout = setTimeout(() => {
-                    this.command.requestStatus();
-                }, opts.pollingInterval);
+                // MPD changed, but has only notified us of the change.  Request the
+                // status and don't run handler until the status is received.
+                if (this.isChanged(msg)) {
+                    return this.responseOkay(msg) && this.command.requestStatus();
+                }
+
+                // Run callback with the new status
+                handler(this.data.status);
+
+                // Request the status on the given interval, or idle and wait for messages
+                if (opts.polling && this.state.isPlaying()) {
+                    timeout = setTimeout(() => {
+                        this.command.requestStatus();
+                    }, opts.pollingInterval);
+                } else {
+                    this.command.idle();
+                    clearTimeout(timeout);
+                }
+            });
+        } catch (error: any) {
+            if (error.message === "Port 6600 already in use") {
+                console.log(error.message);
             } else {
-                this.command.idle();
-                clearTimeout(timeout);
+                throw error;
             }
-        });
+        }
+    }
+
+    private async handleRefusedConnection(refusedConnections: number): Promise<void> {
+        const connection = await this.connect(++refusedConnections);
+
+        if (connection) {
+            this.handler && this.listen(this.handler);
+        } else {
+            if (refusedConnections === 1) {
+                // TODO: Event for refused connection
+            }
+
+            // dev
+            console.log("refused to connect");
+        }
     }
 }
 
